7d09ba7741694cc4f7afa35f795258069bb22b0f
Fernando Henrique <fernando.henrique14@outlook.com>
2025-04-12 10:00:51 -0300

    First
    Inicializando os estudos.

---
+++ screen-match.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Bem vindos ao Screen Match\n";
+
+$nomeFilme = "Terminator 2 - Judgement Day"; // string (texto)
+$anoFilme = 2022;
+$notaFilme = 8.7; // float (número com ponto)
+$inclusoNoPlano = true; // boolean (true ou false)
+
+echo "Nome do filme: $nomeFilme\n";
+echo "Ano do filme: $anoFilme\n";
+echo "Nota do filme: $notaFilme\n";
+echo "Incluso no plano: $inclusoNoPlano\n";
+
+?>
+
+
+
+
+
+
+
+
---
+++ string-numerica.php
@@ -0,0 +1,2 @@
+<?php
+echo '22' +  '20';  //O PHP consegue somar strings numéricas, mas não consegue somar strings não numéricas.
