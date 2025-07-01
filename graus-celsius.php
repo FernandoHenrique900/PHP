e92a69587bfa00ba96cbb8cbd10a0861055b8593
Fernando Henrique <fernando.henrique14@outlook.com>
2025-07-01 00:32:45 -0300

    CRUD-feat
    Projeto de CRUD em PHP.

--- graus-celsius.php
+++ graus-celsius.php
@@ -4,10 +4,12 @@ Crie uma função em PHP que converta graus celsius para Fahrenheit.
 */
 
 function converterCelsiusParaFahrenheit ($celsius): float {
-    return $f = ($celsius*(9/5))+32;
+    return $f = $celsius*(9/5)+32;
 }
 
-$celsius = 32;
-$f = converterCelsiusParaFahrenheit(32);
+$celsius = 10; // Exemplo de temperatura em graus Celsius
+// Chamada da função para converter Celsius para Fahrenheit
+// e exibe o resultado
+$f = converterCelsiusParaFahrenheit($celsius);
 echo "$celsius ºC equivalem a $f º Fahrenheit\n";
 ?>
